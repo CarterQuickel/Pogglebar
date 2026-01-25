e69cfb38707e4633bcc953c5a27a133821b9651f
RoyalCookiez <27CQuickel@live.ytech.edu>
2026-01-24 19:16:45 -0500

    Update class_dec.js

--- modules/backend_js/tb_declar/class_dec.js
+++ modules/backend_js/tb_declar/class_dec.js
@@ -1,34 +1,62 @@
 const classes = [
     {
-        name: "",
-        description: ""
+        //full-support
+        name: "Lifebearer",
+        description: "Full of heart made from bliss, any inflicted wounds shall be cleansed in this bearer's hands."
+        /* Passives:
+        - +30% healing done when an ally is under 50% HP
+        - 15% HP restored when an ally uses an ultimate
+        - Cleanses allies of elemental inflictions when healing
+        */
     },
     {
-        name: "",
+        //mid-damage
+        name: "Vengeance",
         description: ""
+        /* Passives:
+        - +1% damage every turn (resets after being inflicted with an element)
+        - Damage done to enemies decreases their healing received by 15%
+        - +10% speed when under 30% HP
+        */
     },
     {
-        name: "",
+        //full-damage
+        name: "Abyssal",
         description: ""
+        /* Passives:
+        - HP cannot be healed past 50%
+        - -1% HP every turn
+        - +300% damage to single-targeted enemies
+        */
     },
     {
-        name: "",
+        //mid-damage
+        name: "Reaper",
         description: ""
+        /* Passives:
+        - +2% HP per 100 damage done
+        - +35% HP per enemy killed
+        - +5% damage when healed, which lasts a turn
+        */
     },
     {
-        name: "",
+        //full-tank
+        name: "Bulwark",
         description: ""
+        /* Passives:
+        - Resets defense when killing an enemy
+        - +1% damage per 10% defense
+        - Getting single-target healed sets defense to 0
+        */
     },
     {
-        name: "",
+        //mid-support
+        name: "Solar",
         description: ""
-    },
-    {
-        name: "",
-        description: ""
-    },
-    {
-        name: "",
-        description: ""
+        /* Passives:
+        - +10% damage received to an enemy when attacked
+        - +40% healing done by AOE abilities
+        - -15% damage taken by elemental attacks
+        */
     }
 ]
